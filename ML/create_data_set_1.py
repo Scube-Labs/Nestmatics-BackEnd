4f25cc784cf5ee33fd556f78d6a2b01a0d78729a
Pedro Santiago <pedro.santiago16@upr.edu>
2020-10-29 16:50:58 -0400

    Create input features for maya data.

--- ML/create_data_set_1.py
+++ ML/create_data_set_1.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from feature_makers import make_rides_features, make_precipitation_features, make_temperature_features
-from feature_fetchers import fetch_historical_precipitation_data, fetch_historical_temperature_data
+from feature_makers import *
+from feature_fetchers import *
 import numpy as np
 import datetime
 
@@ -47,8 +47,29 @@ for month in range(1, 13):
 
         # with open(str(rides_of_day.iloc[0]['date']) + '.npy', 'wb') as f:
         #     np.save(f, r)
+
         df_temp = pd.DataFrame([(str(rides_of_day.iloc[0]['date']), str(rides_of_day.iloc[0]['date']) + '.npy', p[0][0], t[0][0])], columns=['date', 'rides_file_name', 'precipitation', 'temperature'])
-        training_data.append(df_temp, ignore_index=True)
+        training_data = training_data.append(df_temp, ignore_index=True)
 
 print(training_data)
 training_data.to_csv('training_data.csv')
+
+#TODO auto
+map_divisions = 2
+
+lats = np.linspace(top, bottom, map_divisions+1)
+lons = np.linspace(right, left, map_divisions+1)
+
+data = {"elements":[]}
+
+for lat_index in range(0, len(lats)-1):
+    for lon_index in range(0, len(lons)-1):
+        block = fetch_terrain_data(lats[lat_index], lats[lat_index+1], lons[lon_index], lons[lon_index+1])
+        data["elements"] = data["elements"] + block["elements"] 
+
+img_streets, img_buildings, img_amenities = make_terrain_features(data, top, bottom, right, left)
+
+img_streets.save('street.bmp')
+img_buildings.save('buildings.bmp')
+for key in img_amenities:
+    img_amenities[key].save(str(key) + '.bmp')
