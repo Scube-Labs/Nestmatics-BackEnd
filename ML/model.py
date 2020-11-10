c95c03db37df76c5eefaaf40e9cbb76e9b057820
Pedro Santiago <pedro.santiago16@upr.edu>
2020-11-09 21:40:26 -0500

    Current working version with ~60% validation accu.

--- ML/model.py
+++ ML/model.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from PIL import Image
 import numpy as np
 from tensorflow.keras import Input
 from tensorflow.keras import Model
@@ -8,24 +7,26 @@ from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import UpSampling2D
-import skimage.measure
+from tensorflow.keras.layers import LeakyReLU
+import tensorflow as tf
+import csv
+from data_sequencer import DataSequencer
 
 class NestmaticModel(tf.keras.Model):
+
     def __init__(self):
         super(NestmaticModel, self).__init__()
 
-        self.input_height = 128
-        self.input_width = 128
-        self.input_depth = 15
-
-        # self.inputs = Input(shape=(self.input_height, self.input_width , self.input_depth))
-
-        self.conv1 = Conv2D(32, (4, 4), activation='relu', padding='same')
-        self.conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')
-        self.conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')
-        self.conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')
-        self.conv5 = Conv2D(32, (3, 3), activation='relu', padding='same')
-        self.conv6 = Conv2D(24, (3, 3), activation='relu', padding='same')
+        self.conv1 = Conv2D(32, (4, 4), activation=LeakyReLU(), padding='same')
+        self.conv1_2 = Conv2D(32, (4, 4), activation=LeakyReLU(), padding='same')
+        self.conv2 = Conv2D(64, (3, 3), activation=LeakyReLU(), padding='same')
+        self.conv2_2 = Conv2D(64, (3, 3), activation=LeakyReLU(), padding='same')
+        self.conv3 = Conv2D(128, (3, 3), activation=LeakyReLU(), padding='same')
+        self.conv3_2 = Conv2D(128, (3, 3), activation=LeakyReLU(), padding='same')
+        self.conv4 = Conv2D(64, (3, 3), activation=LeakyReLU(), padding='same')
+        self.conv4_2 = Conv2D(64, (3, 3), activation=LeakyReLU(), padding='same')
+        self.conv5 = Conv2D(32, (3, 3), activation=LeakyReLU(), padding='same')
+        self.conv6 = Conv2D(24, (3, 3), activation=LeakyReLU(), padding='same')
 
         
         self.dropout = Dropout(0.2)
@@ -33,66 +34,117 @@ class NestmaticModel(tf.keras.Model):
 
     def call(self, inputs, training=False):
         #Encoder
-        print(inputs)
         conv1 = self.conv1(inputs)
         if training:
             conv1 = self.dropout(conv1)
-        pool1 = self.pool(conv1)
+        conv1_2 = self.conv1_2(conv1)
+        if training:
+            conv1_2 = self.dropout(conv1_2)
+        pool1 = self.pool(conv1_2)
+
         conv2 = self.conv2(pool1)
         if training:
             conv2 = self.dropout(conv2)
-        pool2 = self.pool(conv2)
+        conv2_2 = self.conv2_2(conv2)
+        if training:
+            conv2_2 = self.dropout(conv2_2)
+        pool2 = self.pool(conv2_2)
+
         conv3 = self.conv3(pool2)
         if training:
             conv3 = self.dropout(conv3)
+        conv3_2 = self.conv3_2(conv3)
+        if training:
+            conv3_2 = self.dropout(conv3_2)
+        
         #Decoder
-        up1 = concatenate([UpSampling2D((2, 2))(conv3), conv2], axis=-1)
+        up1 = concatenate([UpSampling2D((2, 2))(conv3_2), conv2_2], axis=-1)
         conv4 = self.conv4(up1)
         if training:
             conv4 = self.dropout(conv4)
-        up2 = concatenate([UpSampling2D((2, 2))(conv4), conv1], axis=-1)
+        conv4_2 = self.conv4_2(conv4)
+        if training:
+            conv4_2 = self.dropout(conv4_2)
+        
+        up2 = concatenate([UpSampling2D((2, 2))(conv4_2), conv1_2], axis=-1)
         conv5 = self.conv5(up2)
         if training:
             conv5 = self.dropout(conv5)
-        conv6 = self.conv6(conv5)#TODO add emporal and weather stuff here
+        conv6 = self.conv6(conv5)
         return conv6
 
 
-rides = np.load('/home/pedro/nestmatics/Nestmatics-BackEnd/ML/data_sets/1/2019-08-12.npy')
-streets = np.asarray(Image.open('/home/pedro/nestmatics/Nestmatics-BackEnd/ML/data_sets/1/street.bmp'))
-buldings = np.asarray(Image.open('/home/pedro/nestmatics/Nestmatics-BackEnd/ML/data_sets/1/buildings.bmp'))
-education = np.asarray(Image.open('/home/pedro/nestmatics/Nestmatics-BackEnd/ML/data_sets/1/Education.bmp'))
-entertainment = np.asarray(Image.open('/home/pedro/nestmatics/Nestmatics-BackEnd/ML/data_sets/1/Entertainment.bmp'))
-healthcare = np.asarray(Image.open('/home/pedro/nestmatics/Nestmatics-BackEnd/ML/data_sets/1/Healthcare.bmp'))
-substance = np.asarray(Image.open('/home/pedro/nestmatics/Nestmatics-BackEnd/ML/data_sets/1/Substenance.bmp'))
-transportation = np.asarray(Image.open('/home/pedro/nestmatics/Nestmatics-BackEnd/ML/data_sets/1/Transportation.bmp'))
-other = np.asarray(Image.open('/home/pedro/nestmatics/Nestmatics-BackEnd/ML/data_sets/1/other.bmp'))
+def custom_loss_function(y_actual, y_predicted):
+    """Custom loss function that takes into acount failing non zero values more. It is based on MSE.
 
-print(rides.shape) #1242, 1157, 24
-print(streets.shape) #5785, 6210
-ix = int(streets.shape[0]/2)
-iy = int(streets.shape[1]/2)
+    Args:
+        y_actual (tensor): Tensor with true output value.
+        y_predicted (tensor): Tensor with predicte output.
 
-x = np.dstack([
-    np.full((128,128,7), 2).astype(np.float16),
-    skimage.measure.block_reduce(streets[ix:ix+(128*5), iy:iy+(128*5)], (5,5), func=np.max).astype(np.float16), 
-    skimage.measure.block_reduce(buldings[ix:ix+(128*5), iy:iy+(128*5)], (5,5), func=np.max).astype(np.float16), 
-    skimage.measure.block_reduce(education[ix:ix+(128*5), iy:iy+(128*5)], (5,5), func=np.max).astype(np.float16), 
-    skimage.measure.block_reduce(entertainment[ix:ix+(128*5), iy:iy+(128*5)], (5,5), func=np.max).astype(np.float16), 
-    skimage.measure.block_reduce(healthcare[ix:ix+(128*5), iy:iy+(128*5)], (5,5), func=np.max).astype(np.float16), 
-    skimage.measure.block_reduce(substance[ix:ix+(128*5), iy:iy+(128*5)], (5,5), func=np.max).astype(np.float16), 
-    skimage.measure.block_reduce(transportation[ix:ix+(128*5), iy:iy+(128*5)], (5,5), func=np.max).astype(np.float16), 
-    skimage.measure.block_reduce(other[ix:ix+(128*5), iy:iy+(128*5)], (5,5), func=np.max).astype(np.float16)
-    ])
+    Returns:
+        tensor: Loss value
+    """
+    loss = tf.where(y_actual > 0, (y_actual-y_predicted)*3, (y_actual-y_predicted)) # It pennalises 3 times a missed ride.
+    loss = tf.keras.backend.sum(tf.keras.backend.square(loss))
+    return loss
 
-y = rides[int(ix/5):int(ix/5)+128, int(iy/5):int(iy/5)+128]
 
-x = np.array([x])
-y = np.array([y])
 
-model = NestmaticModel()
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(x, y,epochs=20, batch_size=1)
-model.evaluate(x, y)
-model.summary()
-model.save_weights('my_model.h5')
+def predict(model_path, x): 
+    """[summary]
+
+    Args:
+        model_path (str): Path of h5 model weights for the nestmatic model.
+        x (numpy array): [description]
+
+    Returns:
+        [type]: [description]
+    """
+
+    model = NestmaticModel()
+    model.compile(loss=custom_loss_function, optimizer='adam', metrics=['accuracy'])
+    
+    model.predict(np.zeros((1,128,128,20))) # Need to make a prediction to instanciate model to load the weights
+    model.load_weights(model_path)
+
+    #TODO multiple predictions
+
+    res = model.predict(x.reshape((1,128,128,20)))
+    return res
+
+
+def train(train_csv, validation_csv, model_path, new_model_path, batch_size=64, epochs=10, augmentation=True):
+
+    train = list(csv.reader(open(train_csv)))
+    validate = list(csv.reader(open(validation_csv)))
+
+    train_sequencer = DataSequencer(train, batch_size=batch_size, blur=5, augmentation=augmentation)
+    validation_sequencer = DataSequencer(validate, batch_size=256, blur=5)
+
+    model = NestmaticModel()
+    model.compile(loss=custom_loss_function, optimizer='adam', metrics=['accuracy'])
+
+    model.predict(np.zeros((1,128,128,20))) # Need to make a prediction to instanciate model to load the weights
+    model.load_weights(model_path)
+
+    model.fit(train_sequencer, validation_data=validation_sequencer, epochs=epochs, shuffle=True)
+
+    model.save_weights(new_model_path)
+
+    return new_model_path
+
+
+def validate(x, y, model_path):
+    model = NestmaticModel()
+    model.compile(loss=custom_loss_function, optimizer='adam', metrics=['accuracy'])
+
+    model.predict(np.zeros((1,128,128,20))) # Need to make a prediction to instanciate model to load the weights
+    model.load_weights(model_path)
+
+    return model.evaluate(x.reshape((1,128,128,20)), y.reshape((1,128,128,24)))
+
+
+
+
+# train = list(csv.reader(open('/home/pedro/nestmatics/Nestmatics-BackEnd/ML/data_sets/3/train.csv')))
+# validate = list(csv.reader(open('/home/pedro/nestmatics/Nestmatics-BackEnd/ML/data_sets/3/validation.csv')))
